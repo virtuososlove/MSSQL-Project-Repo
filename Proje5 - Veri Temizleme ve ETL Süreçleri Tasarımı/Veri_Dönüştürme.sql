636b7a9bf77d53397ef1d00697356392f1b81132
Melih Üzel <111230329+virtuososlove@users.noreply.github.com>
2025-05-19 22:03:53 +0300

    Add files via upload

---
+++ Proje5 - Veri Temizleme ve ETL Süreçleri Tasarımı/Veri_Dönüştürme.sql
@@ -0,0 +1,59 @@
+--Müþteri Ýsimlerini Büyük Harfe Dönüþtürme
+USE WideWorldImporters;
+GO
+
+SELECT TOP 10
+    CustomerID,
+    CustomerName AS OrjinalIsim,
+    UPPER(CustomerName) AS BuyukHarfIsim
+FROM
+    Sales.Customers;
+GO
+
+-- Sales.Customers tablosundaki CustomerName sütununu büyük harfe güncellenmesi
+UPDATE Sales.Customers
+SET CustomerName = UPPER(CustomerName);
+GO
+
+-- Güncellemenin etkisini görmek için ilk 10 kaydý tekrar seçilmesi
+SELECT TOP 10
+    CustomerID,
+    CustomerName
+FROM
+    Sales.Customers;
+GO
+
+-- Müþterilerin teslimat adresi bileþenlerini birleþtirerek tam adresi oluþtur (Ýlk 10)
+SELECT TOP 10
+    C.CustomerID,
+    C.CustomerName,
+    CONCAT(
+        C.DeliveryAddressLine1,
+        CASE
+            WHEN C.DeliveryAddressLine2 IS NOT NULL AND C.DeliveryAddressLine2 <> ''
+            THEN N', ' + C.DeliveryAddressLine2
+            ELSE N''
+        END,
+        N', ', CT.CityName,
+        N', ', SP.StateProvinceName,
+        N' ', C.DeliveryPostalCode
+    ) AS TamTeslimatAdresi
+FROM
+    Sales.Customers AS C
+INNER JOIN
+    Application.Cities AS CT ON C.DeliveryCityID = CT.CityID
+INNER JOIN
+    Application.StateProvinces AS SP ON CT.StateProvinceID = SP.StateProvinceID;
+GO
+
+-- Müþterilerin hesap açýlýþ tarihinden yýl bilgisini çýkar (Ýlk 10)
+SELECT TOP 10
+    CustomerID,
+    CustomerName,
+    AccountOpenedDate,
+    YEAR(AccountOpenedDate) AS HesapAcilisYili
+FROM
+    Sales.Customers
+ORDER BY
+    AccountOpenedDate;
+GO
